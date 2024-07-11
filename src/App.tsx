08368df5d741965823a49312d5852ffe314de710
Giambi <nash9328@hotmail.com>
2024-07-11 14:23:09 +0800

    fix: unuesd code

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WebApp from '@twa-dev/sdk'
 import { TonConnectUI } from '@tonconnect/ui';
 
 function App() {
